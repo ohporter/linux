ffa8da52f3143b4da7ec50b5f9dbaa74f4ddd4b6
Matt Porter <mporter@ti.com>
2012-09-17 12:39:20 -0400

    message fixup
    Signed-off-by: Matt Porter <mporter@ti.com>

--- drivers/spi/spi-omap2-mcspi.c
+++ drivers/spi/spi-omap2-mcspi.c
@@ -1224,7 +1224,8 @@ static int __devinit omap2_mcspi_probe(struct platform_device *pdev)
 
 	pinctrl = devm_pinctrl_get_select_default(&pdev->dev);
 	if (IS_ERR(pinctrl))
-		dev_warn(&pdev->dev, "unable to select pingroup\n");
+		dev_warn(&pdev->dev,
+			"pins are not configured from the driver\n");
 
 	pm_runtime_use_autosuspend(&pdev->dev);
 	pm_runtime_set_autosuspend_delay(&pdev->dev, SPI_AUTOSUSPEND_TIMEOUT);
